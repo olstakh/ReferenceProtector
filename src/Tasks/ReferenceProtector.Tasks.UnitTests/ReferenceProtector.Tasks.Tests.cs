b9e1e20d97c478c226fa79da92e3b9c646f52296
Alex S <olstakh@microsoft.com>
2025-08-02 11:58:52 -0700

    Initial commit

---
+++ src/Tasks/ReferenceProtector.Tasks.UnitTests/ReferenceProtector.Tasks.Tests.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+
+namespace ReferenceProtector.Tasks.Tests;
+
+public class CollectAllReferencesTests
+{
+    [Fact]
+    public void Execute_ShouldSetOutputMessage_WhenOutputFileIsProvided()
+    {
+        // Arrange
+        var task = new CollectAllReferences
+        {
+            OutputFile = "references.txt"
+        };
+
+        // Act
+        var result = task.Execute();
+
+        // Assert
+        Assert.True(result);
+        Assert.Equal("references.txt", task.OutputMessage);
+    }
+
+    [Fact]
+    public void Execute_ShouldSetOutputMessageToN_A_WhenOutputFileIsNull()
+    {
+        // Arrange
+        var task = new CollectAllReferences();
+
+        // Act
+        var result = task.Execute();
+
+        // Assert
+        Assert.True(result);
+        Assert.Equal("N/A", task.OutputMessage);
+    }
+}
---
+++ src/Tasks/ReferenceProtector.Tasks/CollectAllReferences.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Build.Framework;
+
+namespace ReferenceProtector.Tasks;
+
+/// <summary>
+/// A task to collect all references in the project.
+/// </summary>
+public class CollectAllReferences : Microsoft.Build.Utilities.Task
+{
+    [Required]
+    public string? OutputFile { get; set; }
+
+    [Output]
+    public string OutputMessage { get; set; } = string.Empty;
+
+    public override bool Execute()
+    {
+        OutputMessage = OutputFile ?? "N/A";
+        return true;
+    }
+}
