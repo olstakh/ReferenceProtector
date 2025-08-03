9729ae352425616d8343b8c274d5e421863dd408
Alex S <olstakh@microsoft.com>
2025-08-03 15:02:31 -0700

    Build inside bin folder

--- src/Tasks/ReferenceProtector.Tasks.IntegrationTests/ReferenceProtector.Tasks.Tests.cs
+++ src/Tasks/ReferenceProtector.Tasks.IntegrationTests/ReferenceProtector.Tasks.Tests.cs
@@ -4,35 +4,125 @@ using Xunit;
 
 namespace ReferenceProtector.Tasks.Tests;
 
-public class CollectAllReferencesIntegrationTests
+public class CollectAllReferencesIntegrationTests : IDisposable
 {
-    private readonly int _randomSeed = Environment.TickCount;
     private readonly ITestOutputHelper _output;
-    private readonly Random _random;
-
+    private readonly string _testDirectory;
 
     public CollectAllReferencesIntegrationTests(ITestOutputHelper output)
     {
         _output = output;
+        _testDirectory = SetupTestEnvironment();
+        _output.WriteLine($"Test Directory Created: {_testDirectory}");
+    }
 
-        _output.WriteLine($"Random Seed: {_randomSeed}");
-        _random = new Random(_randomSeed);
+    internal static string SetupTestEnvironment()
+    {
+        var identifier = Guid.NewGuid();
+        var testDirectory = Path.Combine(Directory.GetCurrentDirectory(), identifier.ToString());
+        Directory.CreateDirectory(testDirectory);
+
+        File.WriteAllText(Path.Combine(testDirectory, "dirs.proj"), """
+<?xml version="1.0" encoding="utf-8"?>
+<Project Sdk="Microsoft.Build.Traversal">
+    <ItemGroup>
+        <ProjectReference Include="**\dirs.proj" />
+        <ProjectReference Include="**\*.csproj" />
+    </ItemGroup>
+</Project>
+""");
+
+        File.WriteAllText(Path.Combine(testDirectory, "Directory.Build.props"), """
+<Project>
+  <Import Project="..\ReferenceProtector.Build.props" />
+</Project>
+""");
+
+        File.WriteAllText(Path.Combine(testDirectory, "Directory.Build.targets"), """
+<Project>
+  <Import Project="..\ReferenceProtector.Build.targets" />
+</Project>
+""");
+
+        return testDirectory;
+    }
+
+    internal static void CreateProject(string testDirectory, string projectName)
+    {
+        var projectPath = Path.Combine(testDirectory, projectName);
+        Directory.CreateDirectory(projectPath);
+        File.WriteAllText(Path.Combine(projectPath, $"{projectName}.csproj"), """
+<Project Sdk="Microsoft.NET.Sdk">
+  <PropertyGroup>
+    <TargetFramework>net9.0</TargetFramework>
+  </PropertyGroup>
+</Project>
+""");
+
+        File.WriteAllText(Path.Combine(projectPath, $"Class.cs"), $@"
+namespace {projectName};
+
+public class Class1
+{{
+}}
+");
+    }
+
+    internal static async Task AddProjectReference(string testDirectory, string projectName, string referenceProjectName, ITestOutputHelper logger)
+    {
+        var projectPath = Path.Combine(testDirectory, projectName, $"{projectName}.csproj");
+        var referencePath = Path.Combine(testDirectory, referenceProjectName, $"{referenceProjectName}.csproj");
+
+        var process = Process.Start(new ProcessStartInfo
+        {
+            FileName = "dotnet",
+            Arguments = "add " + projectPath + " reference " + referencePath,
+            WorkingDirectory = testDirectory,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        });
+
+        Assert.NotNull(process);
+        Assert.False(process.HasExited);
+
+        string output = await process.StandardOutput.ReadToEndAsync();
+        string error = await process.StandardError.ReadToEndAsync();
+
+        await process.WaitForExitAsync();
+
+        logger.WriteLine($"MSBuild Output: {output}");
+        logger.WriteLine($"MSBuild Error: {error}");
     }
 
     [Fact]
     public async Task CollectAllReferences_ShouldCollectReferences()
     {
-        _output.WriteLine("Starting CollectAllReferences integration test...");
-
-        await RunMSBuildAsync();
-
-        Assert.Fail("boo");
+        CreateProject(_testDirectory, "TestProject1");
+        CreateProject(_testDirectory, "TestProject2");
+        CreateProject(_testDirectory, "TestProject3");
+        await AddProjectReference(_testDirectory, "TestProject1", "TestProject2", _output);
+        await AddProjectReference(_testDirectory, "TestProject2", "TestProject3", _output);
+        await RunMSBuildAsync(_testDirectory, _output);
     }
 
-    private async Task RunMSBuildAsync()
+    public void Dispose()
+    {
+        _output.WriteLine("Disposing CollectAllReferencesIntegrationTests...");
+
+        // Clean up the test directory
+        if (Directory.Exists(_testDirectory))
+        {
+            Directory.Delete(_testDirectory, true);
+            _output.WriteLine($"Test Directory Deleted: {_testDirectory}");
+        }
+    }
+
+    internal static async Task RunMSBuildAsync(string testDirectory, ITestOutputHelper logger)
     {
         var currentDirectory = Directory.GetCurrentDirectory();
-        _output.WriteLine($"Current Directory: {currentDirectory}");
+        logger.WriteLine($"Current Directory: {currentDirectory}");
 
         string buildArgs =
             $"build dirs.proj " +
@@ -41,12 +131,12 @@ public class CollectAllReferencesIntegrationTests
             $"/p:ReferenceProtectorTaskAssembly={Path.Combine(Directory.GetCurrentDirectory(), "ReferenceProtector.Tasks.dll")} " +
             $"/v:m";
 
-        _output.WriteLine($"Running MSBuild with arguments: {buildArgs}");
+        logger.WriteLine($"Running MSBuild with arguments: {buildArgs}");
         var process = Process.Start(new ProcessStartInfo
         {
             FileName = "dotnet",
             Arguments = buildArgs,
-            WorkingDirectory = Path.Combine(Directory.GetCurrentDirectory(), "Manual"),
+            WorkingDirectory = testDirectory,
             RedirectStandardOutput = true,
             RedirectStandardError = true,
             UseShellExecute = false,
@@ -54,14 +144,16 @@ public class CollectAllReferencesIntegrationTests
         });
 
         Assert.NotNull(process);
+        Assert.False(process.HasExited);
 
-        process.Start();
         string output = await process.StandardOutput.ReadToEndAsync();
         string error = await process.StandardError.ReadToEndAsync();
 
         await process.WaitForExitAsync();
 
-        _output.WriteLine($"MSBuild Output: {output}");
-        _output.WriteLine($"MSBuild Error: {error}");
+        logger.WriteLine($"MSBuild Output: {output}");
+        logger.WriteLine($"MSBuild Error: {error}");
+
+        Assert.Equal(0, process.ExitCode);
     }
 }
